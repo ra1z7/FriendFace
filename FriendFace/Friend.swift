7b69ec7cacb2f56ebb5f88a220ddfb3c35147044
ra1z <purnamaanrai7@gmail.com>
2025-12-26 18:11:54 +0545

    Create User and Friend types

---
+++ FriendFace/Friend.swift
@@ -0,0 +1,13 @@
+//
+//  Friend.swift
+//  FriendFace
+//
+//  Created by Purnaman Rai (College) on 26/12/2025.
+//
+
+import Foundation
+
+struct Friend {
+    let id: UUID
+    let name: String
+}
