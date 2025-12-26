907cc63e3c07a15a28befe080e30e4b8568ec754
ra1z <purnamaanrai7@gmail.com>
2025-12-26 18:03:51 +0545

    Initial Commit

---
+++ FriendFace/FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceApp.swift
+//  FriendFace
+//
+//  Created by Purnaman Rai (College) on 26/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
