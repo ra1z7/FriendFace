a82dde3fdf5c2f575a3c1160421c5845fa003a7a
ra1z <purnamaanrai7@gmail.com>
2026-01-03 20:47:02 +0545

    Added Offline screen when user is not connected to internet and download is failed

--- FriendFace/ContentView.swift
+++ FriendFace/ContentView.swift
@@ -9,29 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var allUsers = [User]()
+    @State private var userIsOffline = false
     
     var body: some View {
         NavigationStack {
-            List(allUsers) { user in
-                NavigationLink(value: user) {
-                    HStack(spacing: 14) {
-                        Image(systemName: "person.crop.circle.fill")
-                            .foregroundStyle(Color.random.gradient)
-                            .font(.largeTitle)
-                            .imageScale(.large)
-                        
-                        VStack(alignment: .leading) {
-                            Text(user.name)
-                                .font(.headline)
-                            
-                            HStack {
-                                Circle()
-                                    .fill(user.isActive ? Color.green.gradient : Color.gray.gradient)
-                                    .frame(width: 10, height: 10)
+            Form {
+                if userIsOffline {
+                    ContentUnavailableView("You're Offline", systemImage: "wifi.slash")
+                } else {
+                    List(allUsers) { user in
+                        NavigationLink(value: user) {
+                            HStack(spacing: 14) {
+                                Image(systemName: "person.crop.circle.fill")
+                                    .foregroundStyle(Color.random.gradient)
+                                    .font(.largeTitle)
+                                    .imageScale(.large)
                                 
-                                Text(user.isActive ? "Online" : "Offline")
-                                    .font(.caption)
-                                    .foregroundStyle(.gray)
+                                VStack(alignment: .leading) {
+                                    Text(user.name)
+                                        .font(.headline)
+                                    
+                                    HStack {
+                                        Circle()
+                                            .fill(user.isActive ? Color.green.gradient : Color.gray.gradient)
+                                            .frame(width: 10, height: 10)
+                                        
+                                        Text(user.isActive ? "Online" : "Offline")
+                                            .font(.caption)
+                                            .foregroundStyle(.gray)
+                                    }
+                                }
                             }
                         }
                     }
@@ -49,6 +56,8 @@ struct ContentView: View {
             if allUsers.isEmpty {
                 if let fetchedUsers = await User.fetchUsers() {
                     allUsers = fetchedUsers
+                } else {
+                    userIsOffline = true
                 }
             }
         }
